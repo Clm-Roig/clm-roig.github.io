e96c32962f51f0f2fb59a09b1ff8e2bf12f8a10c
Clément Roig <clm.roig@gmail.com>
2022-10-06 21:44:29 +0200

    Initial Vue 3 setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import SkillsView from "./SkillsView.vue";
+import ProjectsView from "./ProjectsView.vue";
+</script>
+
+<template>
+  <main>
+    <section class="wrapper">
+      <h1><strong>Hi, I'm Clément Roig</strong>I'm a front-end developer!</h1>
+      <img class="gravatar" src="../assets/images/gravatar.jpg" />
+    </section>
+
+    <SkillsView />
+    <ProjectsView />
+  </main>
+</template>
+
+<style scoped style="scss">
+.gravatar {
+  width: 100px;
+}
+.wrapper {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+}
+h1 {
+  text-align: center;
+}
+h1 > strong {
+  display: block;
+  font-size: 150%;
+}
+</style>
---
+++ src/views/SkillsView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Skills</h2>
+  <ul>
+    <li>React</li>
+    <li>Jest</li>
+    <li>Cypress</li>
+    <li>MUI</li>
+
+    <li>CSS</li>
+    <li>HTML</li>
+
+    <li>Git</li>
+    <li>Github Actions</li>
+
+    <li>Sails.js</li>
+    <li>Node.js</li>
+
+    <li>Postgres</li>
+    <li>SQL</li>
+    <li>MongoDB</li>
+
+    <li>Adobe Photoshop</li>
+    <li>Adobe Premiere</li>
+  </ul>
+</template>
+
+<style scoped style="scss"></style>
