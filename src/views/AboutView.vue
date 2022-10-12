3a6071f0958f5dc6414e03b431ee93de8374c5c3
Clément <clm.roig@gmail.com>
2022-10-12 12:10:54 +0200

    about & contact section (first draft)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,23 +1,68 @@
 <script setup lang="ts">
-import gravatar from "@/assets/images/gravatar_no_bg.png";
+import gravatar from "@/assets/images/texture_green_silver.jpg";
 </script>
 
 <template>
   <h2>About me</h2>
-  <div class="wrapper">
-    <img class="gravatar" :src="gravatar" />
-    <p>Hi, I'm Clément Roig!</p>
+  <figure class="portrait">
+    <img class="portrait__gravatar" :src="gravatar" />
+    <figcaption class="portrait__caption">Hi, I'm Clément Roig!</figcaption>
+  </figure>
+
+  <div class="about-me">
+    <p class="about-me__text">
+      I'm a human being and also a web developer. I like to build
+      <b
+        >user-friendly and beautiful web interfaces but also useful and
+        practical REST API for other developers</b
+      >. I am constantly learning and improving my self to enhance my code
+      quality: refactoring, reducing repetitions, testing and documenting my
+      code is a <u>core part of my workflow</u>.
+    </p>
+    <p class="about-me__text">
+      I am thriving by developing solution for meaningful organizations, which
+      work on fields like
+      <b
+        >education, health, protection of the environment, climate change
+        fighting or knowledge sharing</b
+      >.
+    </p>
+    <p class="about-me__text">
+      When I'm not pixel-perfecting a web interface or pushing an API
+      refactoring on GitHub, I'm making music at the
+      <a href="https://onyx-studio.fr" rel="noopener noreferrer">Onyx Studio</a
+      >. You will also find me hiking in the Ariège Pyrenees (South of France),
+      picking mushrooms or chestnuts.
+    </p>
   </div>
 </template>
 
 <style scoped lang="scss">
-.gravatar {
-  width: 200px;
-  height: auto;
-}
-.wrapper {
-  display: flex;
+.portrait {
   align-items: center;
+  display: flex;
   flex-direction: column;
+  &__gravatar {
+    height: auto;
+    width: 200px;
+  }
+
+  &__caption {
+    background-color: transparentize($black, 0.3);
+    bottom: 40px;
+    left: 40px;
+    margin-bottom: -20px; /* remove empty space remaining after moving the block */
+    position: relative;
+  }
+}
+
+.about-me {
+  &__text {
+    text-indent: 16px;
+    margin-top: 16px;
+  }
+  &__text:first-child {
+    margin-top: 0;
+  }
 }
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,7 +2,45 @@
 
 <template>
   <h2>Contact me</h2>
-  <div class="placeholder-block">PLACEHOLDER BLOCK</div>
+  <div class="contact">
+    <p><a href="mailto:clm.roig@gmail.com">clm.roig@gmail.com</a></p>
+    <a
+      class="contact__link"
+      href="https://www.linkedin.com/in/clm-roig/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <font-awesome-icon
+        class="contact__icon"
+        size="2xl"
+        :icon="['fa-brands', 'fa-linkedin']"
+      ></font-awesome-icon>
+    </a>
+    <a
+      class="contact__link"
+      href="https://github.com/clm-roig/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <font-awesome-icon
+        class="contact__icon"
+        size="2xl"
+        :icon="['fa-brands', 'fa-github']"
+      ></font-awesome-icon>
+    </a>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.contact {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  &__link:hover {
+    background-color: transparent;
+    color: $white;
+    scale: 1.1;
+  }
+}
+</style>
