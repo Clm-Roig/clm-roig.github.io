ffaa22e1b4cff6ae7ec6bc0d7dbde9c2720333b8
Clément Roig <clm.roig@gmail.com>
2022-10-13 12:18:09 +0200

    Improve contact section

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,31 +3,34 @@
 <template>
   <h2>{{ $t("contact.section-title") }}</h2>
   <div class="contact">
+    <p>{{ $t("contact.text") }}</p>
     <p><a href="mailto:clm.roig@gmail.com">clm.roig@gmail.com</a></p>
-    <a
-      class="contact__link"
-      href="https://www.linkedin.com/in/clm-roig/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <font-awesome-icon
-        class="contact__icon"
-        size="2xl"
-        :icon="['fa-brands', 'fa-linkedin']"
-      ></font-awesome-icon>
-    </a>
-    <a
-      class="contact__link"
-      href="https://github.com/clm-roig/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <font-awesome-icon
-        class="contact__icon"
-        size="2xl"
-        :icon="['fa-brands', 'fa-github']"
-      ></font-awesome-icon>
-    </a>
+    <div class="contact__icons">
+      <a
+        class="contact__link"
+        href="https://www.linkedin.com/in/clm-roig/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <font-awesome-icon
+          class="contact__icon"
+          size="2xl"
+          :icon="['fa-brands', 'fa-linkedin']"
+        ></font-awesome-icon>
+      </a>
+      <a
+        class="contact__link"
+        href="https://github.com/clm-roig/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <font-awesome-icon
+          class="contact__icon"
+          size="2xl"
+          :icon="['fa-brands', 'fa-github']"
+        ></font-awesome-icon>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -37,6 +40,10 @@
   display: flex;
   flex-direction: column;
   gap: 16px;
+  &__icons {
+    display: flex;
+    gap: 16px;
+  }
   &__link:hover {
     background-color: transparent;
     color: $white;
