b90d978dfb5f0b7da1720c2bd9d94f1b42fcae8d
Clément Roig <clm.roig@gmail.com>
2022-10-22 22:01:19 +0200

    Format GrottocenterView to use same layout as SuivieView

--- src/views/projects/GrottocenterView.vue
+++ src/views/projects/GrottocenterView.vue
@@ -1,40 +1,71 @@
 <script setup lang="ts">
 import { projects } from "@/models/projects";
+import Section from "../../components/Section.vue";
 const { grottocenter } = projects;
 </script>
 
 <template>
-  <div class="header dark-bg">
-    <h1>Grottocenter</h1>
-    <a :href="grottocenter.url" target="_blank" rel="noopener noreferrer">{{
-      grottocenter.url
-    }}</a>
-  </div>
-  <section class="under-construction">
+  <Section dark-bg class="back-to-projects">
+    <router-link
+      class="link link--fancy"
+      :to="{ path: '/', hash: '#projects' }"
+    >
+      {{ "< " + $t("back-to-projects") }}
+    </router-link>
+  </Section>
+  <header class="header">
+    <h1>
+      <img
+        class="logo"
+        src="/src/assets/images/grottocenter/grottocenter-logo.png"
+        alt="Grottocenter logo"
+      />
+    </h1>
+    <a
+      class="link link--fancy"
+      :href="grottocenter.url"
+      target="_blank"
+      rel="noopener noreferrer"
+      >{{ grottocenter.url }}</a
+    >
+  </header>
+  <Section class="under-construction">
     <p>⚠ {{ $t("under-construction") }} ⚠</p>
     <router-link class="link link--fancy" to="/">{{
       $t("back-to-projects")
     }}</router-link>
-  </section>
+  </Section>
 </template>
 
 <style scoped lang="scss">
-$img-gap: 1rem;
+$flex-gap: 1rem;
+
+.header {
+  padding: 16px 0;
+  text-align: center;
+}
 
 .under-construction {
   background-color: $secondary-color;
   font-size: 2rem;
-  margin: 64px auto;
+  margin: 32px auto;
   padding: 1rem;
   text-align: center;
   width: fit-content;
 }
 
-.header {
-  text-align: center;
+.logo {
+  margin: auto;
+  width: 50vw;
+  max-width: 200px;
 }
 
-.dark-bg {
-  background-color: $black;
+p {
+  margin-bottom: 16px;
+}
+
+.back-to-projects {
+  padding: 32px 0;
+  text-align: center;
 }
 </style>
