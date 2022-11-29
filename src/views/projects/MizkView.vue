e939c93b8262b8ed302e9d7e1f9d2ad4fb3e046c
Clément Roig <clm.roig@gmail.com>
2022-11-29 14:52:29 +0100

    add details to mizk project

--- src/views/projects/MizkView.vue
+++ src/views/projects/MizkView.vue
@@ -2,11 +2,15 @@
 import { projects } from "@/models/projects";
 import Section from "../../components/Section.vue";
 import BackToProjects from "../../components/BackToProjects.vue";
-// import { AVAILABLE_PROJECTS, getImage } from "../../utils/getImage";
+import ImageList from "../../components/ImageList.vue";
+import { AVAILABLE_PROJECTS, getImage } from "../../utils/getImage";
 const { mizk } = projects;
 
-// const getMizkImage = (imageName: string) =>
-//   getImage(imageName, AVAILABLE_PROJECTS.mizk);
+const getMizkImage = (imageName: string) =>
+  getImage(imageName, AVAILABLE_PROJECTS.mizk);
+
+const imageList1 = [getMizkImage("menu"), getMizkImage("colors")];
+const imageList2 = [getMizkImage("image-crop")];
 </script>
 
 <template>
@@ -23,7 +27,41 @@ const { mizk } = projects;
   </header>
 
   <Section dark-bg>
-    <h2>Page en construction...</h2>
+    <h2>{{ $t("presentation") }}</h2>
+    <div class="content">
+      <div class="content__text">
+        <p>{{ $t("projects.mizk.details.part1") }}</p>
+      </div>
+      <ImageList :images="imageList1" />
+    </div>
+  </Section>
+
+  <Section>
+    <h2>{{ $t("features") }}</h2>
+    <div class="content content--images-first content--align-center">
+      <ImageList :images="imageList2" />
+      <div class="content__text">
+        <ul class="classic-ul">
+          <li>{{ $t("projects.mizk.details.feat1") }}</li>
+          <li>{{ $t("projects.mizk.details.feat2") }}</li>
+          <li>{{ $t("projects.mizk.details.feat3") }}</li>
+          <li>{{ $t("projects.mizk.details.feat4") }}</li>
+          <li>{{ $t("projects.mizk.details.feat5") }}</li>
+        </ul>
+      </div>
+    </div>
+  </Section>
+
+  <Section dark-bg>
+    <h2>{{ $t("technical-overview") }}</h2>
+    <div class="content">
+      <div class="content__text">
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <p v-html="$t('projects.mizk.details.tech1')"></p>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <p v-html="$t('projects.mizk.details.tech2')"></p>
+      </div>
+    </div>
   </Section>
 
   <BackToProjects />
